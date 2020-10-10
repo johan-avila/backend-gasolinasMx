0b14854b691d52f85c5390b2603b5763a194afab
johan-avila <johan-avila@outlook.com>
2020-10-10 18:39:00 -0500

    listo

--- services/stations.js
+++ services/stations.js
@@ -4,13 +4,13 @@ const MongoLib= require("../lib/mongodb")
 class StationsService {
     constructor(){
         this.mongodb= new MongoLib()
-        this.collection = "stations"
+        this.collection = "places"
     }
     async getAll({estado}){
         
         const query = estado ? { "estado.estado.short_name":estado } : {}
         const stations =await this.mongodb.getAll(this.collection, query)
-        console.log(stations);
+        
         return stations           
     };
     async getOne(_id){
